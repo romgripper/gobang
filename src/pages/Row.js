1801b6d2a1a95a567e1323a79d4943c1c80f8657
Bin Xing <xb1101101@gmail.com>
2023-08-04 22:49:27 +1200

    move Square to a separate file

--- src/pages/Row.js
+++ src/pages/Row.js
@@ -1,21 +1,6 @@
 import Gobang from "./Gobang";
 
-function Square({ square, positionStyle, onSquareClick }) {
-    function shouldBlink(square) {
-        return square.isLatestMove() || square.isIn5() || square.isInOpen3() || square.isInOpen4();
-    }
-
-    let className = "square";
-    if (square.isMarkedByPlayer()) {
-        className += square.isBlack() ? " black" : " white";
-        className += positionStyle;
-        if (shouldBlink(square)) className += "-blink";
-    } else {
-        className += " empty" + positionStyle;
-    }
-
-    return <button className={className} onClick={onSquareClick} />;
-}
+import Square from "./Square";
 
 export function Row({ squares, row, handleClick }) {
     const columns = [];
---
+++ src/pages/Square.js
@@ -0,0 +1,16 @@
+export default function Square({ square, positionStyle, onSquareClick }) {
+    function shouldBlink(square) {
+        return square.isLatestMove() || square.isIn5() || square.isInOpen3() || square.isInOpen4();
+    }
+
+    let className = "square";
+    if (square.isMarkedByPlayer()) {
+        className += square.isBlack() ? " black" : " white";
+        className += positionStyle;
+        if (shouldBlink(square)) className += "-blink";
+    } else {
+        className += " empty" + positionStyle;
+    }
+
+    return <button className={className} onClick={onSquareClick} />;
+}

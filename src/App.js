02eb05857e234c1040ddd121bd88fc3de283e135
Bin Xing <xb1101101@gmail.com>
2023-08-15 19:01:37 +1200

    Fix image preload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import StateProvider from "./GameStateContext";
 import Board from "./Board";
 import "./App.css";
-import PreloadImageBoard from "./PreloadImageBoard";
 
 export default function App() {
     return (
         <>
-            <PreloadImageBoard isBlack={true} />
-            <PreloadImageBoard isBlack={false} />
             <StateProvider>
                 <Board />
             </StateProvider>

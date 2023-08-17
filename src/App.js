3e28f0fcde81c72fa8d52860eb385607de91ac12
Bin Xing <xb1101101@gmail.com>
2023-08-17 23:20:12 +1200

    Use query param to choose between go and gobang as router is not working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import StateProvider from "./GameStateContext";
 import Board from "./Board";
 import "./App.css";
 
-export default function App({ gameName }) {
+export default function App() {
+    const gameName = new URLSearchParams(window.location.search).get("game") ?? "gobang";
     return (
         <>
             <StateProvider gameName={gameName}>
--- src/index.js
+++ src/index.js
@@ -3,26 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App gameName="gobang" />
-    },
-    {
-        path: "/gobang",
-        element: <App gameName="gobang" />
-    },
-    {
-        path: "/go",
-        element: <App gameName="go" />
-    }
-]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>
 );
